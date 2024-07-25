6ae273badb117bdf06c6e17e62c05a9d406b0180
박승호 <joinus0409@naver.com>
2024-07-25 16:38:51 +0900

    feat: 통화내역 저장 테이블 추가

--- Back/app/models.py
+++ Back/app/models.py
@@ -1,8 +1,9 @@
 from sqlalchemy import Column, Integer, String, Boolean, Float, Double
 from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
-class user_info():
+class user_info(Base):
     __tablename__ = 'user_info'
 
     user_id = Column(String(255), primary_key=True)
@@ -12,7 +13,7 @@ class user_info():
     last_modified_date = Column(String(255))
     profile_image = Column(String(255))
 
-class is_friend():
+class is_friend(Base):
     __tablename__ = 'is_friend'
 
     friend_id = Column(String(255), primary_key=True)
@@ -23,12 +24,24 @@ class is_friend():
     is_friend = Column(Boolean)
     shared_album_id = Column(String(255))
 
-class phone_info():
+class phone_info(Base):
     __tablename__ = 'phone_info'
 
-    phone_id = Column(String(255), primary_key=True)
+    phone_id = Column(Integer, index = True,primary_key=True)
     user_id = Column(String(255))
     name = Column(String(255))
     phone = Column(String(255))
     create_date = Column(String(255))
     last_modified_date = Column(String(255))
+
+class call_record_info(Base):
+    __tablename__ = 'call_record_info'
+
+    call_id = Column(Integer, index = True, primary_key=True)
+    user_id = Column(String(255))
+    name = Column(String(255))
+    phone = Column(String(255))
+    date = Column(String(255))
+    time = Column(String(255))
+    duration = Column(String(255))
+    type = Column(Integer)
