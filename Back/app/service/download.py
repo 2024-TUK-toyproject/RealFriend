8669056c07b68f5b9f86f6f8a73b1534a5c5cd2a
박승호 <joinus0409@naver.com>
2024-07-25 16:39:52 +0900

    feat: 마지막 통화내역 반환 관련 코드 모듈화

---
+++ Back/app/service/download.py
@@ -0,0 +1,30 @@
+from fastapi import HTTPException, status, Depends
+from sqlalchemy.orm import Session
+
+
+from ..database import Database
+from .. import models
+from ..schemes import *
+from datetime import datetime
+from pytz import timezone
+
+class download_service:
+    def __init__(self, db: Session = Depends(Database().get_session)):
+        self.db = db
+        self.today = datetime.now(timezone('Asia/Seoul'))
+
+    async def get_last_call(self, user_id : str) -> Last_call_response:
+        last_call = self.db.query(models.call_record_info).filter(models.call_record_info.user_id == user_id).order_by(models.call_record_info.date.desc()).first()
+        if last_call is None:
+            raise HTTPException(status_code=400, detail="통화 기록이 없습니다.")
+        
+        return Last_call_response(
+            name = last_call.name,
+            phone = last_call.phone,
+            date = last_call.date,
+            time = last_call.time,
+            duration = last_call.duration,
+            type = last_call.type
+        )
+
+
