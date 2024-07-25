6dca70f410d5234451e52539090569472b171b14
박승호 <joinus0409@naver.com>
2024-07-25 21:30:31 +0900

    feat: 프로필 설정 관련 요청 응답 스키마 생성

--- Back/app/schemes.py
+++ Back/app/schemes.py
@@ -4,7 +4,7 @@ from typing import List, Dict, Optional
 
 # Response Model
 class CommoneResponse(BaseModel):
-    status : str = Field("success", example = "success")
+    status : str = Field("success", example = "success or error")
     message : str = Field("성공메시지 or 오류메시지")
 
 class Last_call_response(BaseModel):
@@ -14,6 +14,11 @@ class Last_call_response(BaseModel):
     time : str = Field(..., example = "14:20:01")
     duration : str = Field(..., example = "00:01:30")
     type : int = Field(..., example = "1", description="1 : 수신, 2 : 발신, 3 : 부재중")
+
+class Profile_response(BaseModel):
+    name : str = Field(..., example = "홍길동")
+    phone : str = Field(..., example = "010-1234-5678")
+    profile_image : str = Field(..., example = "https://s3.ap-northeast-2.amazonaws.com/album-app/123456/123456.jpg")
 
 # Request Model
 class Phone_list(BaseModel):
@@ -34,3 +39,12 @@ class Call_record_list(BaseModel):
 class Call_record_request(BaseModel):
     userId : str = Field(..., example = "123456")
     content : List[Call_record_list]
+
+class User_info_request(BaseModel):
+    name : str = Field(..., example = "홍길동")
+    phone : str = Field(..., example = "010-1234-5678")
+    
+class Set_profile_request(BaseModel):
+    userId : str = Field(..., example = "123456")
+    
+
