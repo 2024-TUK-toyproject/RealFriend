83dd395bd73ff73b64ec1b9ba6b936e95e3587fd
박승호 <joinus0409@naver.com>
2024-07-26 20:24:42 +0900

    fix: user_id 판별 추가

--- Back/app/service/download.py
+++ Back/app/service/download.py
@@ -14,6 +14,9 @@ class download_service:
         self.today = datetime.now(timezone('Asia/Seoul'))
 
     async def get_last_call(self, user_id : str) -> Last_call_response:
+        
+        self.check_user_id(user_id)
+
         last_call = self.db.query(models.call_record_info).filter(models.call_record_info.user_id == user_id).order_by(models.call_record_info.date.desc()).first()
         if last_call is None:
             raise HTTPException(status_code=400, detail="통화 기록이 없습니다.")
@@ -26,5 +29,9 @@ class download_service:
             duration = last_call.duration,
             type = last_call.type
         )
-
+    
+    def check_user_id(self, user_id : str):
+        user = self.db.query(models.user_info).filter(models.user_info.user_id == user_id).first()
+        if user is None:
+            raise HTTPException(status_code=400, detail="사용자 정보가 없습니다.")
 
