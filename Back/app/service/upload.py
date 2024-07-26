74a52bf241f0ddadcb9e64e90fe097913841516b
박승호 <joinus0409@naver.com>
2024-07-26 20:25:34 +0900

    fix: user_id 판별 추가, 통화내역 동기화 후 통화 횟수가 많은 상위 3명 리턴

--- Back/app/service/upload.py
+++ Back/app/service/upload.py
@@ -15,6 +15,9 @@ class upload_service:
         self.today = datetime.now(timezone('Asia/Seoul'))
 
     async def upload_phone_info(self, phone_list : Phone_request) -> CommoneResponse:
+
+        self.check_user_id(phone_list.userId)
+        
         for phone in phone_list.content:
             new_phone = models.phone_info(
                 user_id = phone_list.userId,
@@ -28,7 +31,9 @@ class upload_service:
 
         return CommoneResponse(status = "success", message = "연락처 동기화 성공")
     
-    async def upload_call_record(self, call_record_list : Call_record_request) -> CommoneResponse:
+    async def upload_call_record(self, call_record_list : Call_record_request) -> Call_record_response:  
+        
+        self.check_user_id(call_record_list.userId)
 
         for call_record in call_record_list.content:
             date1, time = self.divide_date(call_record.date)
@@ -38,14 +43,40 @@ class upload_service:
                 phone = call_record.phone,
                 date = date1,
                 time = time, 
-                duration = call_record.duration,
+                duration = self.preprocess_duration(call_record.duration),
                 type = call_record.type
             )
             self.db.add(new_call_record)
         
         self.db.commit()
 
-        return CommoneResponse(status = "success", message = "통화 기록 동기화 성공")
+        #최대 3개월 이내의 통화내역가져온다
+        call_record = self.db.query(models.call_record_info).filter(models.call_record_info.user_id == call_record_list.userId).filter(models.call_record_info.date >= self.today.strftime('%Y-%m-%d')).all()
+
+        #같은 번호로 통화한 횟수와 통화시간을 구한다
+        phone_dict = {}
+        for record in call_record:
+            if record.phone in phone_dict:
+                phone_dict[record.phone]['count'] += 1
+                phone_dict[record.phone]['duration'] += record.duration
+            else:
+                phone_dict[record.phone] = {'count' : 1, 'duration' : record.duration}
+        
+        #통화횟수가 많은 순으로 정렬
+        phone_dict = sorted(phone_dict.items(), key = lambda x : x[1]['count'], reverse = True)
+
+        content = []
+
+        for phone in phone_dict:
+            content.append(Most_friendly_list(
+                name = phone[0],
+                phone = phone[0],
+                duration = phone[1]['duration'],
+                count = phone[1]['count']
+            ))
+
+        
+        return Call_record_response(status = "success", message = "통화 기록 동기화 성공", content = content)
     
     def divide_date(self, date):
         #2024.11.20.14.20.01
@@ -60,3 +91,18 @@ class upload_service:
         date1 = f"{year}-{month}-{day}"
         time = f"{hour}:{minute}:{second}"
         return date1, time
+    
+    def preprocess_duration(self, duration):
+        #00:01:30
+        #시간 분 초 분할
+        duration = duration.split(':')
+        hour = duration[0]
+        minute = duration[1]
+        second = duration[2]
+        return int(hour)*3600 + int(minute)*60 + int(second)
+        
+    def check_user_id(self, userId):
+        user = self.db.query(models.user_info).filter(models.user_info.user_id == userId).first()
+        if user is None:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="사용자 정보가 없습니다.")
+        return user
