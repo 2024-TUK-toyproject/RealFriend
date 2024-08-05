ff1d966da1ef1dd8bf379d01df0813a577aa2613
박승호 <joinus0409@naver.com>
2024-08-05 19:54:18 +0900

    feat: 공유앨범 목록 조회, 공유앨범 생성 함수 모듈화

---
+++ Back/app/service/album.py
@@ -0,0 +1,54 @@
+from fastapi import HTTPException, status, Depends, UploadFile
+from sqlalchemy.orm import Session
+
+
+from ..database import Database
+from .. import models
+from ..schemes import *
+from ..random_generator import RandomNumberGenerator
+from ..config import Config
+
+
+class Album_service:
+    def __init__(self, db: Session = Depends(Database().get_session)):
+        self.db = db
+        self.rng = RandomNumberGenerator()
+
+    async def get_album_list(self, userId : str) -> Album_list_response:
+        album_list1 = self.db.query(models.album_info).filter(models.album_info.create_user_id == userId).all()
+        album_list2 = self.db.query(models.album_info).filter(models.album_info.with_whom == userId).all()
+
+        album_list = album_list1 + album_list2
+
+        album_list_response = []
+        for album in album_list:
+            album_list_response.append({"albumId" : album.album_id, "albumName" : album.album_name})
+
+        return Album_list_response(status = "success", message = "앨범 리스트 조회 성공", content = album_list_response)
+
+    async def create_album(self, request : Album_create_request) -> Album_create_response:
+        valified_user1 = self.db.query(models.is_friend).filter(models.is_friend.from_user_id == request.userId, models.is_friend.to_user_id == request.withWhom).first()
+        valified_user2 = self.db.query(models.is_friend).filter(models.is_friend.from_user_id == request.withWhom, models.is_friend.to_user_id == request.userId).first()
+
+        '''existing_album = self.db.query(models.album_info).filter(models.album_info.create_user_id == request.userId, models.album_info.with_whom == request.withWhom).first()
+        if existing_album is not None:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="이미 존재하는 앨범입니다.")'''
+        
+        if valified_user1.is_friend == False or valified_user2.is_friend == False:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="친구가 아닙니다.")
+        
+        for _ in range(10):
+            new_key = self.rng.generate_unique_random_number(100000, 999999)
+
+        new_album = models.album_info(
+            album_id = new_key,
+            album_name = request.albumName,
+            create_user_id = request.userId,
+            with_whom = request.withWhom,
+            directory = f"sharedAlbum/{new_key}"
+        )
+
+        self.db.add(new_album)
+        self.db.commit()
+
+        return Album_create_response(status = "success", message = "앨범 생성 성공", content = {"albumId" : new_key, "albumName" : request.albumName})
