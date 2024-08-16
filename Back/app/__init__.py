80d7f3eb2ec910b4d4e101afe9f04b6f8d96bc06
박승호 <joinus0409@naver.com>
2024-08-16 23:59:03 +0900

    fix: connex 0.0.2 버전업

--- Back/app/__init__.py
+++ Back/app/__init__.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 
 app = FastAPI(
     title = "Connex", 
-    description="Connex API 문서",
-    version="0.0.1"
+    description="Connex API 문서\n어떤 api든 응답으로 토큰 만료가 뜨면 재로그인 필요",
+    version="0.0.2"
     )
 
 def create_app():
