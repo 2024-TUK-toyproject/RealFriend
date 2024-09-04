67e94bcf64be90a12773189e6c996ffc88da4d67
박승호 <joinus0409@naver.com>
2024-09-04 17:01:02 +0900

    fix: v0.0.3

--- Back/app/__init__.py
+++ Back/app/__init__.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 app = FastAPI(
     title = "Connex", 
     description="Connex API 문서\n어떤 api든 응답으로 토큰 만료가 뜨면 재로그인 필요",
-    version="0.0.2"
+    version="0.0.3"
     )
 
 def create_app():
